be1a6c0b63a459d298b702743fe87c8db7cec7e7
chase <chase.reckling@gmail.com>
2015-06-12 12:28:46 -0700

    add answer section and 'new answer ' button.

--- router.js
+++ router.js
@@ -2,6 +2,6 @@ App.Router.map(function() {
   this.resource('questions', {path: '/'});
   this.resource('new-question');
   this.resource('question', {path: '/:question_id'}, function() {
+    this.resource('new-answer');
   });
-    //  this.resource('new-answer');
    });

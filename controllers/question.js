09e8d8c42906fd711c5d4b8ac898ed47ceeeb8b0
chase <chase.reckling@gmail.com>
2015-06-12 11:53:05 -0700

    allow user to click on a question link from the index page, will display the individual question page.

---
+++ controllers/question.js
@@ -0,0 +1,17 @@
+App.QuestionController = Ember.ObjectController.extend({
+  isEditing: false,
+  actions: {
+    delete:function() {
+      if(confirm('Are you sure?')){
+        this.get('model').destroyRecord();
+        this.transitionToRoute('questions');
+      }
+    },
+    edit: function() {
+      this.set('isEditing', true);
+    },
+    save: function() {
+      this.set('isEditing', false);
+    }
+  }
+});
